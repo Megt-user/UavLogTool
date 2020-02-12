7b19c2a844db2aa95a4bed1caa9631a276b823d6
Matias Gonzalez Tognoni <megt_user@hotmail.com>
2020-02-12 19:08:00 +0100

    Add Helpers and csv Utilities

---
+++ UavLogTool/CsvUtilities.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UavLogTool.Models;
+
+namespace UavLogTool
+{
+    public class CsvUtilities
+    {
+        public static Dictionary<string, int> GetDjiHeaderDictionary(string[] headres)
+        {
+            var djiLogs = UavLog.GetDJIMappingProperty();
+            var headerDictionary = new Dictionary<string, int>();
+            for (var i = 0; i < headres.Count(); i++)
+            {
+
+                foreach (var djilog in djiLogs)
+                {
+                    if (headres[i] == djilog.Value)
+                    {
+                        headerDictionary.Add(djilog.Key, i);
+                    }
+
+                }
+            }
+            return headerDictionary;
+        }
+
+        public static UavLog GetUavLog(string[] fields, Dictionary<string, int> headers)
+        {
+            var uavLog = new UavLog();
+            var djiLog = UavLog.GetDJIMappingProperty();
+
+
+            uavLog.FlyTime = fields[headers["FlyTime"]];
+            uavLog.UavLatititud = fields[headers["UavLatititud"]];
+            uavLog.UavLongitud = fields[headers["UavLongitud"]];
+            uavLog.VideoRecordTime = fields[headers["VideoRecordTime"]];
+            var pointDateTime = fields[headers["DateTime"]];
+
+            DateTime dateTime;
+            var formatStrings = new string[] { "yyyy/MM/dd HH:mm:ss.fff", "yyyy-MM-dd hh:mm:ss", "yyyy-MM-dd", "mm:ss.f" };
+            CultureInfo enUS = new CultureInfo("en-US");
+
+            if (DateTime.TryParseExact(pointDateTime, formatStrings, enUS, DateTimeStyles.None, out dateTime))
+                uavLog.DateTime = dateTime;
+
+            return uavLog;
+        }
+
+
+        //public static string ToCsv<UavLog>(string separator, IEnumerable<UavLog> objectlist)
+        public static string[] ToCsv<UavLog>(string separator, IEnumerable<UavLog> objectlist)
+        {
+            Type t = typeof(UavLog);
+            //FieldInfo[] fields = t.GetFields();
+            FieldInfo[] fields = t.GetFields(BindingFlags.Instance | BindingFlags.NonPublic);
+
+            string header = String.Join(separator, fields.Select(f => f.Name).ToArray());
+
+            StringBuilder csvdata = new StringBuilder();
+            csvdata.AppendLine(header);
+            List<string> lines = new List<string>();
+            lines.Add(header);
+            foreach (var o in objectlist)
+            {
+                var objectLine = ToCsvFields(separator, fields, o);
+                lines.Add(objectLine);
+                csvdata.AppendLine(ToCsvFields(separator, fields, o));
+
+            }
+
+            //return csvdata.ToString();
+            return lines.ToArray();
+        }
+
+        public static string ToCsvFields(string separator, FieldInfo[] fields, object o)
+        {
+            StringBuilder linie = new StringBuilder();
+
+            foreach (var f in fields)
+            {
+                if (linie.Length > 0)
+                    linie.Append(separator);
+
+                var x = f.GetValue(o);
+
+                if (x != null)
+                    linie.Append(x.ToString());
+            }
+
+            return linie.ToString();
+        }
+
+        public static bool SaveCsvTofile(string path, string[] csvString)
+        {
+            try
+            {
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(path))
+                {
+                    foreach (string line in csvString)
+                        file.WriteLine(line);
+                }
+
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+    }
+}
---
+++ UavLogTool/Helpers.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using UavLogTool.Models;
+
+namespace UavLogTool
+{
+    public class Helpers
+    {
+        public static UavLog GetPositionScreemshotPostionFromVideo(string photoTimeStamp, List<UavLog> uavLogs)
+        {
+            UavLog uavLog = null;
+            var formatStrings = new string[] { @"mm\:ss\.ff", @"mm\:ss\:ff", @"mm\:ss" };
+            TimeSpan imageTimeStamp;
+            if (TimeSpan.TryParseExact(photoTimeStamp, formatStrings, null, out imageTimeStamp))
+            {
+                try
+                {
+                    var videoLenght = GetVideoLenghtInSeconds(uavLogs);
+                    if (imageTimeStamp.TotalMilliseconds < videoLenght)
+                    {
+                        var firstVideoLog = uavLogs.Select(l => l.DateTime).Min();
+                        var photoTimeInVideo = firstVideoLog + imageTimeStamp;
+
+                        //https://stackoverflow.com/a/7016646
+                        var nearestDiff = uavLogs.Min(log => Math.Abs((log.DateTime - photoTimeInVideo).Ticks));
+                        var nearest = uavLogs.First(log => Math.Abs((log.DateTime - photoTimeInVideo).Ticks) == nearestDiff);
+
+                        //https://stackoverflow.com/a/4075380
+                        var uavLogIndex = uavLogs.Select((value, index) => new { value, index })
+                            .First(log => Math.Abs((log.value.DateTime - photoTimeInVideo).Ticks) == nearestDiff);
+
+                        uavLog = uavLogIndex.value;
+                        var LatLong = $"{nearest.UavLatititud},{nearest.UavLongitud}"; //"59.409424,9.132055"
+                        var dif1 = ConvertMilisecondsToHMSmm(TimeSpan.FromTicks(nearestDiff).TotalMilliseconds);
+                    }
+                }
+                catch
+                {
+
+                    //return null;
+                }
+            }
+            return uavLog;
+        }
+
+
+        public static Dictionary<int, List<UavLog>> SplitVideosFromUavLog(List<UavLog> uavLogs)
+        {
+            var videoLogs = new Dictionary<int, List<UavLog>>();
+
+            var uavlogsTemp = new List<UavLog>();
+            string previus = "1";
+            bool flag = false;
+            int videoNumber = 1;
+            string logName = "VideoRecordTime";
+
+            foreach (var log in uavLogs)
+            {
+                var actual = log.VideoRecordTime;
+                if (previus == "1" && previus != actual && !flag)
+                {
+                    flag = true;
+                }
+                //else
+                //{
+                //    if (flag && actual == "1")
+                //    {
+                //        videoNumber++;
+                //        previus = actual;
+                //        flag = false;
+                //        logName = $"{logName}_{videoNumber}";
+                //    }
+                //}
+
+                if (flag && actual == "1")
+                {
+                    videoLogs.Add(videoNumber, uavlogsTemp);
+                    videoNumber++;
+                    previus = actual;
+                    flag = false;
+                    uavlogsTemp = new List<UavLog>();
+                }
+                log.VideoNumber = videoNumber;
+                uavlogsTemp.Add(log);
+            }
+            videoLogs.Add(videoNumber, uavlogsTemp);
+
+            return videoLogs;
+        }
+
+        public static double GetVideoLenghtInSeconds(List<UavLog> uavLogs)
+        {
+
+            var dateTimeFirst = uavLogs.First().DateTime;
+            var dateTimeLast = uavLogs.Last().DateTime;
+            var videoLenght = (dateTimeLast - dateTimeFirst).TotalMilliseconds;
+            return videoLenght;
+        }
+
+        public static string ConvertMilisecondsToHMSmm(double milliseconds)
+        {
+            TimeSpan t = TimeSpan.FromMilliseconds(milliseconds);
+            string answer = string.Format("{0:D2}h:{1:D2}m:{2:D2}s:{3:D3}ms",
+                t.Hours,
+                t.Minutes,
+                t.Seconds,
+                t.Milliseconds);
+
+            return answer;
+        }
+    }
+}
