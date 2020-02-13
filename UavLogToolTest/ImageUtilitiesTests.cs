b6990bf5e415c3bd386d4ddb37164dcdb2fe4533
Matias Gonzalez Tognoni <megt_user@hotmail.com>
2020-02-13 12:08:52 +0100

    wip

--- UavLogToolTest/ImageUtilitiesTests.cs
+++ UavLogToolTest/ImageUtilitiesTests.cs
@@ -1,10 +1,18 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Net.Mime;
 using System.Text;
+using Xunit;
+using  ExifTagManager;
 
 namespace UavLogToolTest
 {
     public class ImageUtilitiesTests
     {
+        [Fact]
+        public void Test1()
+        {
+            var path = @"Data\DJI_0739.JPG";
+        }
     }
 }
