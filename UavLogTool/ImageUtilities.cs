ae5a7c705aacee6bfaa5e1651bd5cfa9346e4906
Matias Gonzalez Tognoni <megt_user@hotmail.com>
2020-02-13 16:28:44 +0100

    wip

--- UavLogTool/ImageUtilities.cs
+++ UavLogTool/ImageUtilities.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,82 @@ namespace UavLogTool
 {
     public class ImageUtilities
     {
+
+        public static byte[] FloatToExifGps(int degrees, int minutes, int seconds)
+        {
+            var secBytes = BitConverter.GetBytes(seconds);
+            var secDivisor = BitConverter.GetBytes(100);
+            byte[] rv = { (byte)degrees, 0, 0, 0, 1, 0, 0, 0, (byte)minutes, 0, 0, 0, 1, 0, 0, 0, secBytes[0], secBytes[1], 0, 0, secDivisor[0], 0, 0, 0 };
+            return rv;
+        }
+
+        //https://gist.github.com/5342/3293802
+        public static void ExtractLocation(string file)
+        {
+            if (file.ToLower().EndsWith("jpg") || file.ToLower().EndsWith("jpeg"))
+            {
+                Image image = null;
+                try
+                {
+                    Console.Title = file;
+
+                    try
+                    {
+                        FileStream fs = new FileStream(file, FileMode.Open, FileAccess.Read);
+                        image = Image.FromStream(fs);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error opening {0} image may be corupted, {1}", file, ex.Message);
+                        return;
+                    }
+
+                    // GPS Tag Names
+                    // http://www.sno.phy.queensu.ca/~phil/exiftool/TagNames/GPS.html
+
+                    // Check to see if we have gps data
+                    if (Array.IndexOf<int>(image.PropertyIdList, 1) != -1 &&
+                        Array.IndexOf<int>(image.PropertyIdList, 2) != -1 &&
+                        Array.IndexOf<int>(image.PropertyIdList, 3) != -1 &&
+                        Array.IndexOf<int>(image.PropertyIdList, 4) != -1)
+                    {
+
+                        string gpsLatitudeRef = BitConverter.ToChar(image.GetPropertyItem(1).Value, 0).ToString();
+                        string latitude = DecodeRational64u(image.GetPropertyItem(2));
+                        string gpsLongitudeRef = BitConverter.ToChar(image.GetPropertyItem(3).Value, 0).ToString();
+                        string longitude = DecodeRational64u(image.GetPropertyItem(4));
+                        Console.WriteLine("{0}\t{1} {2}, {3} {4}", file, gpsLatitudeRef, latitude, gpsLongitudeRef, longitude);
+                    }
+                }
+                catch (Exception ex) { Console.WriteLine("Error processign {0} {1}", file, ex.Message); }
+                finally
+                {
+                    if (image != null) image.Dispose();
+                }
+
+
+            }
+        }
+
+        public static string DecodeRational64u(System.Drawing.Imaging.PropertyItem propertyItem)
+        {
+            uint dN = BitConverter.ToUInt32(propertyItem.Value, 0);
+            uint dD = BitConverter.ToUInt32(propertyItem.Value, 4);
+            uint mN = BitConverter.ToUInt32(propertyItem.Value, 8);
+            uint mD = BitConverter.ToUInt32(propertyItem.Value, 12);
+            uint sN = BitConverter.ToUInt32(propertyItem.Value, 16);
+            uint sD = BitConverter.ToUInt32(propertyItem.Value, 20);
+
+            decimal deg;
+            decimal min;
+            decimal sec;
+            // Found some examples where you could get a zero denominator and no one likes to devide by zero
+            if (dD > 0) { deg = (decimal)dN / dD; } else { deg = dN; }
+            if (mD > 0) { min = (decimal)mN / mD; } else { min = mN; }
+            if (sD > 0) { sec = (decimal)sN / sD; } else { sec = sN; }
+
+            if (sec == 0) return string.Format("{0}° {1:0.#######}'", deg, min);
+            else return string.Format("{0}° {1:0}' {2:0.#######}\"", deg, min, sec);
+        }
     }
 }
--- UavLogToolTest/ImageUtilitiesTests.cs
+++ UavLogToolTest/ImageUtilitiesTests.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Net.Mime;
 using System.Text;
 using Xunit;
-
+using System.Drawing.Imaging;
+using UavLogTool;
 
 namespace UavLogToolTest
 {
@@ -13,6 +15,41 @@ namespace UavLogToolTest
         public void Test1()
         {
             var path = @"Data\DJI_0739.JPG";
+
+            ImageUtilities.ExtractLocation(path);
+
+            Image image1 = Image.FromFile(path);
+            // Start looking at the exif tags, which are PropertyItems
+            //foreach (PropertyItem item in image1.PropertyItems)
+            //{
+            //    var count = image1.PropertyItems.Length;
+            //    var noko = item.Value; // er ...
+            //    var valueString = System.Text.Encoding.Default.GetString(noko);
+
+
+            //    var idInt = item.Id;
+            //    var hex = idInt.ToString("x8");
+            //    var hex8 = idInt.ToString("X");
+               
+            //    if (hex.Contains("0001"))
+            //    {
+                    
+            //    } 
+            //    if (hex.Contains("0002"))
+            //    {
+                    
+            //    } if (hex.Contains("0002"))
+            //    {
+                    
+            //    }
+            //}
+            PropertyItem PropertyTagGpsLongitude = image1.GetPropertyItem(4);
+            var valueString = System.Text.Encoding.Default.GetString(PropertyTagGpsLongitude.Value);
+            PropertyTagGpsLongitude.Value =ImageUtilities.FloatToExifGps(79, 48, 3377);
+            image1.SetPropertyItem(PropertyTagGpsLongitude);
+
+
+            image1.Save(@"C:\Temp\image_Test.JPG");
         }
     }
 }
