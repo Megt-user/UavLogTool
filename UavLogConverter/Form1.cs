c98914f03319a3ded8d23d7c6645060e4853ce5b
Matias <megt_user@hotmail.com>
2020-03-09 22:14:02 +0100

    Start Windows Form Net Core

---
+++ UavLogConverter/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UavLogConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int size = -1;
+            DialogResult result = openFileDialog.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog.FileName;
+                try
+                {
+
+                }
+                catch (IOException)
+                {
+
+                }
+            }
+        }
+    }
+}
